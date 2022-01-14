1ce5234c315c23e128f9638611fdf445847c7524
Dant3s15 <damiansobieraj5@gmail.com>
2022-01-14 22:12:39 +0100

    Old code cleanup

--- src/app/js/script.js
+++ src/app/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 import * as MODEL from './model.js';
+import * as VIEW from './view.js';
 
 //Init
 (() => {
@@ -107,7 +108,7 @@ let btnSegTop = document.querySelector('.btn_segment-top');
 
 //-------------------------
 //TODO
-let pitch = -45; //TODO
+let pitch = 0; //TODO
 let yaw = -45;
 
 //CONTROLER-------------------
@@ -180,45 +181,6 @@ class RenderSide {
     }
 }
 //-----------------------------
-class Cube extends RenderSide {
-    //TODO probably don't need this class
-    //distance from the center in px
-    constructor() {
-        super();
-        this.renderSides();
-    }
-
-    // cubeMovement(direction) {
-    //     if (direction == 'right') {
-    //         this.yaw += 90;
-    //         this.renderSides();
-    //     }
-    //     if (direction == 'left') {
-    //         this.yaw -= 90;
-    //         this.renderSides();
-    //     }
-    //     if (direction == 'bottom') {
-    //         this.pitch -= 90;
-    //         this.renderSides();
-    //     }
-    //     if (direction == 'top') {
-    //         this.pitch += 90;
-    //         this.renderSides();
-    //     }
-    // }
-}
-
-//pieces--------------------------------------------
-
-let piecesArr = [0];
-let piecesPosition = [0];
-//piecesArr[0] = 0;
-// let pBackup = [...piecesArr];
-// for (let i = 1; i <= 54; i++) {
-//     piecesArr[i] = document.getElementById('tile_' + i);
-//     piecesPosition[i] = i;
-//     //     console.log(piecesArr[i]);
-// }
 
 class Pieces extends RenderSide {
     //TODO
@@ -290,15 +252,14 @@ class Pieces extends RenderSide {
     }
 }
 
-// const d3cube = new Cube();
-const piece = new Pieces();
+const CUBE = new Pieces();
 // piece.renderSideEl('tile_1', 'top');
 //variables----------------------------------------
 
-let translateZ = 150;
-let direction = '';
-let spacing = 0;
-let innerSpacing = 0;
+// let translateZ = 150;
+// let direction = '';
+// let spacing = 0;
+// let innerSpacing = 0;
 //css sides variables-------------------------------
 let tilePosition = [
     0,
@@ -312,272 +273,23 @@ let tilePosition = [
     'translateX(0px) translateY(100px)',
     'translateX(100px) translateY(100px)',
 ];
-let whichSide = 0;
-
-// let rotateFaceX = 'rotateX( ' + pitch + 'deg)';
-
-let rotateFaceX = `rotateX( ${pitch}deg)`;
-let rotateFaceY = `rotateY( ${yaw}deg)`;
-let rotateFaceXTop = `rotateX(${pitch + 90}deg)`;
-let rotateFaceXBottom = `rotateX(${pitch - 90}deg)`;
-let rotateFaceYRight = `rotateY(${yaw + 90}deg)`;
-let rotateFaceYBack = `rotateY(${yaw + 180}deg)`;
-let rotateFaceYLeft = `rotateY(${yaw + 270}deg)`;
-let rotateFaceZTop = `rotateZ(${-yaw}deg)`;
-let rotateFaceZBottom = `rotateZ(${+yaw}deg)`;
-
-let rotateFaceZ = 'rotateZ(0deg)';
-let pPopup = 'translateZ(' + (translateZ + spacing) + 'px)';
-
-let sideAngles = [
-    0,
-    document.documentElement.style.setProperty(
-        '--front',
-        rotateFaceX + rotateFaceY
-    ),
-    document.documentElement.style.setProperty(
-        '--right',
-        rotateFaceX + rotateFaceYRight
-    ), //WIP
-    document.documentElement.style.setProperty(
-        '--back',
-        rotateFaceX + rotateFaceYBack
-    ),
-    document.documentElement.style.setProperty(
-        '--left',
-        rotateFaceX + rotateFaceYLeft
-    ),
-    document.documentElement.style.setProperty(
-        '--top',
-        rotateFaceXTop + 'rotateY(0deg)' + rotateFaceZTop
-    ),
-    document.documentElement.style.setProperty(
-        '--bottom',
-        rotateFaceXBottom + 'rotateY(0deg)' + rotateFaceZBottom
-    ),
-];
-
-//getComputedStyle(document.documentElement).getPropertyValue('--top')
-//going to be removed:
-function cubeMovement(direction) {
-    if (direction == 'right') {
-        yaw += 90;
-        turnCube(direction);
-    } else if (direction == 'left') {
-        yaw -= 90;
-        turnCube(direction);
-    } else if (direction == 'bottom') {
-        // alert("move bottom "+rotateFaceX);
-        pitch -= 90;
-        // alert("move bottom "+rotateFaceX);
-        turnCube(direction);
-    } else if (direction == 'top') {
-        pitch += 90;
-        turnCube(direction);
-    } else if (direction == 'movetops') {
-        turnCube(direction);
-    } else
-        alert(
-            "Wrong direction name! Only 'right, left, bottom, top' are allowed!"
-        );
-}
-
-function resetPosition() {
-    yaw = 0;
-    pitch = 0;
-    //WIP
-}
-
-// function getPosition(side) {}
-function variables() {
-    rotateFaceZ = 'rotateZ(0deg)';
-    rotateFaceX = 'rotateX( ' + pitch + 'deg)';
-    rotateFaceXTop = 'rotateX( ' + (pitch + 90) + 'deg)';
-    rotateFaceXBottom = 'rotateX( ' + (pitch - 90) + 'deg)';
-    rotateFaceY = 'rotateY(' + yaw + 'deg)';
-    rotateFaceY0 = 'rotateY(0deg)';
-    rotateFaceXTop = 'rotateX( ' + (pitch + 90) + 'deg)';
-    rotateFaceXBottom = 'rotateX( ' + (pitch - 90) + 'deg)';
-    rotateFaceYRight = 'rotateY(' + (yaw + 90) + 'deg)';
-    rotateFaceYBack = 'rotateY(' + (yaw + 180) + 'deg)';
-    rotateFaceYLeft = 'rotateY(' + (yaw + 270) + 'deg)';
-    rotateFaceZTop = 'rotateZ(' + -yaw + 'deg)';
-    rotateFaceZBottom = 'rotateZ(' + +yaw + 'deg)';
-}
-//going to be removed:
-function turnCube(direction) {
-    // tilePosition = [0,
-    //         "translateX(-100px) translateY(-100px)",//        ^
-    //         "translateX(0px) translateY(-100px)",   //        |-100y
-    //         "translateX(100px) translateY(-100px)", //        |
-    //         "translateX(-100px) translateY(0px)",   //-100x___|0_____100x>
-    //         "translateX(0px) translateY(0px)",      //        |
-    //         "translateX(100px) translateY(0px)",    //        |-100y
-    //         "translateX(-100px) translateY(100px)",
-    //         "translateX(0px) translateY(100px)",
-    //         "translateX(100px) translateY(100px)"];
-
-    // rotateFaceX = "rotateX( "+(pitch)+"deg)";
-    // rotateFaceX = "rotateX( "+(pitch)+"deg)";
-    //rotateFaceY = "rotateY("+(yaw)+"deg)";
-
-    variables();
-    let tile = 1;
-
-    //move inner sides--------------------------------------------
-    // moveFront.style.transform = rotateFaceX + "rotateY("+(yaw)+"deg)" + rotateFaceZ + " translateZ("+(translateZ-innerSpacing)+"px)";
-    // moveRight.style.transform = rotateFaceX + "rotateY("+(yaw+90)+"deg)" +rotateFaceZ + " translateZ("+(translateZ-innerSpacing)+"px)";
-    // moveLeft.style.transform = rotateFaceX + "rotateY("+(yaw-90)+"deg)" + rotateFaceZ + "  translateZ("+(translateZ-innerSpacing)+"px)" ;
-    // moveBack.style.transform = rotateFaceX + "rotateY("+(yaw+180)+"deg)" + rotateFaceZ + " translateZ("+(translateZ-innerSpacing)+"px)" ;
-    // moveTop.style.transform = "rotateX( "+(pitch+90)+"deg) rotateY( 0deg) rotateZ("+(-yaw)+"deg) translateZ("+(translateZ-innerSpacing)+"px)" ;
-    // moveBottom.style.transform = "rotateX("+(pitch-90)+"deg) rotateY( 0deg) rotateZ("+(yaw)+"deg) translateZ("+(translateZ-innerSpacing)+"px)" ;
-
-    //move pieces--------------------------------------------
-
-    if (direction == 'movetops') {
-        piecesArr[1].style.transform =
-            rotateFaceX +
-            rotateFaceYRight +
-            rotateFaceZ +
-            tilePosition[1] +
-            pPopup;
-        piecesArr[2].style.transform =
-            rotateFaceX +
-            rotateFaceYRight +
-            rotateFaceZ +
-            tilePosition[2] +
-            pPopup;
-        piecesArr[3].style.transform =
-            rotateFaceX +
-            rotateFaceYRight +
-            rotateFaceZ +
-            tilePosition[3] +
-            pPopup;
-        piecesArr[28].style.transform =
-            rotateFaceX + rotateFaceY + rotateFaceZ + tilePosition[1] + pPopup;
-        piecesArr[29].style.transform =
-            rotateFaceX + rotateFaceY + rotateFaceZ + tilePosition[2] + pPopup;
-        piecesArr[30].style.transform =
-            rotateFaceX + rotateFaceY + rotateFaceZ + tilePosition[3] + pPopup;
-        piecesArr[10].style.transform =
-            rotateFaceX +
-            rotateFaceYBack +
-            rotateFaceZ +
-            tilePosition[1] +
-            pPopup;
-        piecesArr[11].style.transform =
-            rotateFaceX +
-            rotateFaceYBack +
-            rotateFaceZ +
-            tilePosition[2] +
-            pPopup;
-        piecesArr[12].style.transform =
-            rotateFaceX +
-            rotateFaceYBack +
-            rotateFaceZ +
-            tilePosition[3] +
-            pPopup;
-        piecesArr[19].style.transform =
-            rotateFaceX +
-            rotateFaceYLeft +
-            rotateFaceZ +
-            tilePosition[1] +
-            pPopup;
-        piecesArr[20].style.transform =
-            rotateFaceX +
-            rotateFaceYLeft +
-            rotateFaceZ +
-            tilePosition[2] +
-            pPopup;
-        piecesArr[21].style.transform =
-            rotateFaceX +
-            rotateFaceYLeft +
-            rotateFaceZ +
-            tilePosition[3] +
-            pPopup;
-    }
-
-    if (
-        direction == 'right' ||
-        direction == 'left' ||
-        direction == 'bottom' ||
-        direction == 'top'
-    ) {
-        for (k = 1; k <= 54; k++) {
-            //54
-            //bad ifs?tile = 1;
-            //if (k < 10) rotateFaceY = "rotateY("+(yaw+whichSide)+"deg)";
-
-            if (tile >= 10) tile = 1;
-            piecesArr[k].style.transform =
-                rotateFaceX +
-                rotateFaceY +
-                rotateFaceZ +
-                tilePosition[tile] +
-                pPopup;
-            //if (k < 10 && k>2) rotateFaceY = "rotateY("+(yaw)+"deg)";
-            if (k >= 10) {
-                piecesArr[k].style.transform =
-                    rotateFaceX +
-                    rotateFaceYRight +
-                    rotateFaceZ +
-                    tilePosition[tile] +
-                    pPopup;
-            }
-            if (k >= 19 && k <= 27) {
-                piecesArr[k].style.transform =
-                    rotateFaceX +
-                    rotateFaceYBack +
-                    rotateFaceZ +
-                    tilePosition[tile] +
-                    pPopup;
-            }
-            if (k >= 28 && k <= 36) {
-                piecesArr[k].style.transform =
-                    rotateFaceX +
-                    rotateFaceYLeft +
-                    rotateFaceZ +
-                    tilePosition[tile] +
-                    pPopup;
-            }
-            if (k >= 37) {
-                piecesArr[k].style.transform =
-                    rotateFaceXTop +
-                    rotateFaceY0 +
-                    rotateFaceZTop +
-                    tilePosition[tile] +
-                    pPopup;
-            }
-            if (k >= 46) {
-                piecesArr[k].style.transform =
-                    rotateFaceXBottom +
-                    rotateFaceY0 +
-                    rotateFaceZBottom +
-                    tilePosition[tile] +
-                    pPopup;
-            }
-
-            // tile++;
-        }
-    }
-}
 
 //button event listeners--------------------------------------------
 btnRight.addEventListener('click', function () {
     // cubeMovement('right');
-    piece.cubeMovement('right');
+    CUBE.cubeMovement('right');
 });
 btnLeft.addEventListener('click', function () {
     // cubeMovement('left');
-    piece.cubeMovement('left');
+    CUBE.cubeMovement('left');
 });
 bttile_ottom.addEventListener('click', function () {
     // cubeMovement('bottom');
-    piece.cubeMovement('bottom');
+    CUBE.cubeMovement('bottom');
 });
 btnTop.addEventListener('click', function () {
     // cubeMovement('top');
-    piece.cubeMovement('top');
+    CUBE.cubeMovement('top');
 });
 btnSegTop.addEventListener('click', function () {
     // cubeMovement('movetops');
